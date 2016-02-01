7b50513e8309a62a5c03df79a23777890b6fe9ed
Bryce Farnsworth <bryce.farnsworth@gmail.com>
2016-02-01 14:40:08 -0800

    Finished Stock Picker

--- building_blocks.rb
+++ building_blocks.rb
@@ -35,3 +35,40 @@ puts caesar_cipher(string, 12)
 string = "What a string!"
 puts caesar_cipher(string, 5)
 
+# Project 2: Stock Picker
+
+=begin
+Task:
+Implement a method that takes an array of stock prices, one for each
+day, and returns a pair of days representing the best days to buy and
+sell.
+=end
+
+def stock_picker(prices)
+=begin
+Set prices[0] as min. Go along prices and compare differences,
+recording the maximum price difference as you go. If you reach a new min,
+set it as min instead, and continue comparing differences to the maximum.
+=end
+	min = 0								# Initialize everything to 0
+	buy = 0
+	sell = 0
+	max_diff = 0
+	(1...prices.length).each do |i|
+		if prices[i] < prices[min]
+			# If current price is less than current min:
+			min = i						# Set as current min
+		end
+		diff = prices[i] - prices[min]	# Compare difference to current min
+		if diff > max_diff
+			# If it's better:
+			buy = min					# Record min as day to buy
+			sell = i					# Record current day as day to sell
+			max_diff = diff				# Record new max difference
+		end
+	end
+	[buy, sell]
+end
+
+puts "#{stock_picker([17,3,6,9,15,8,6,1,10])}"
+puts "#{stock_picker([3, 8, 9, 2, 3, 5, 16, 1])}"
